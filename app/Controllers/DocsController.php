f6b8de49cd1ee1030c68d375146b16481e49a49a
mohamed.yousef <engineer.mohamed.yossef@gmail.com>
2026-03-05 05:03:18 +0400

    feat: views done

--- app/Controllers/DocsController.php
+++ app/Controllers/DocsController.php
@@ -90,7 +90,7 @@ class DocsController extends BaseController
 
         // !! For Testing Only !!
         $page->content = $this->renderView(
-            'docs/v0_1_x/views/shared_variables',
+            'docs/v0_1_x/views/error_pages',
             compact('versions', 'hierarchy', 'page')
         );
 
