25cf284fc9aa402b2da843d2288968ac1a841b98
mohamed.yousef <engineer.mohamed.yossef@gmail.com>
2026-03-07 07:13:36 +0400

    feat: db done

--- app/Controllers/DocsController.php
+++ app/Controllers/DocsController.php
@@ -90,7 +90,7 @@ class DocsController extends BaseController
 
         // !! For Testing Only !!
         $page->content = $this->renderView(
-            'docs/v0_1_x/views/error_pages',
+            'docs/v0_1_x/database/seeders',
             compact('versions', 'hierarchy', 'page')
         );
 
